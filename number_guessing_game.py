bf53e70ccfc82ea516d60818741f5c1a260f2703
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-28 03:53:28 +0100

    number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,8 +3,14 @@
 # and asks user to guess them
 
 from random import randint
-# from sys import exit
+print("############### WELCOME TO THE GUESSING GAME ################\n")
 
+print("#####################    HOW TO PLAY    #####################")
+print("# Provide the Upper Bound number you want to guess from\
+        \n# The Game Generates a number from 1 to the number provided\
+        \n# You can now guess the number the game generates\
+        \n# If you are wrong, the game hints you whether your guess\
+        \n# is above or below the Real Number\n")
 
 while True:
     user_number = input("Type a number: ")
@@ -34,19 +40,20 @@ while True:
         print("\t\tINVALID ENTRY!\n\t\tPlease Enter a Number")
         continue
 
-    if user_guess <= 0 or user_guess > user_number :
-        print("\t\tOUT OF BOUNDS!\n\t\tPlease enter a number between 1 and ", user_number)
+    if user_guess <= 0 or user_guess > user_number:
+        msg = "\t\tOUT OF BOUNDS!\n\t\tPlease enter a number between 1 and "
+        print(msg, user_number)
         continue
     else:
         number_guesses += 1
         if user_guess == random_number:
             print("\t\tGREAT! You got it right!")
+            break
         else:
             if user_guess > random_number:
                 print("INCORRECT - above number, Try again!\n")
             elif user_guess < random_number:
                 print("INCORRECT - below number, Try again!\n")
-            continue
-        break
+        continue
 
 print("\t\tYou got it in ", number_guesses, "guesses")
